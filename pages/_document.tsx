8df46e2627bf9170caf67c9abaa02313b4f9bd63
Mohamed Nehad <mohammednehad550@gmail.com>
2022-09-22 02:12:31 +0200

    feat: Add fonts

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <link href="https://fonts.googleapis.com/css?family=Bellefair" rel="stylesheet" type="text/css"></link>
+                <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed " rel="stylesheet" type="text/css"></link>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
