cd945241ffc65c79c530a70455ae1754e043acde
Mohamed Nehad <mohammednehad550@gmail.com>
2022-09-24 08:48:44 +0200

    feat: Add Navbar active state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { NavBar, NavLink, NavLinks } from './components/nav'
-import NavLogo from './components/nav/NavLogo'
+import { NavBar, NavLink, NavLinks } from '../components/nav'
+import NavLogo from '../components/nav/NavLogo'
+import { useRouter } from 'next/router'
+
+const routes = [
+  { href: "/", label: 'Home', prefix: '00' },
+  { href: "/destination", label: 'Destination', prefix: '01' },
+  { href: "/crew", label: 'Crew', prefix: '02' },
+  { href: "/technology", label: 'Technology', prefix: '03' },
+
+]
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const router = useRouter()
 
   return (
     <>
@@ -20,10 +32,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           <div className='border-b border-white border-opacity-25 translate-x-8 w-full z-10' />
         </div>
         <NavLinks>
-          <NavLink href='#' prefix='00' label='Home' active />
-          <NavLink href='#' prefix='01' label='Destination' />
-          <NavLink href='#' prefix='02' label='Crew' />
-          <NavLink href='#' prefix='03' label='Technology' />
+          {routes.map(r => (
+            <NavLink key={r.href} {...{ ...r }} active={r.href === router.pathname} />
+          ))}
         </NavLinks>
       </NavBar>
       <Component {...pageProps} />
