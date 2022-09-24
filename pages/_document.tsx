d59cecf7ac4a0ad177e83ccd785411fd8ca7d178
Mohamed Nehad <mohammednehad550@gmail.com>
2022-09-24 08:48:59 +0200

    feat: Add Barlow font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,8 @@ export default function Document() {
         <Html>
             <Head>
                 <link href="https://fonts.googleapis.com/css?family=Bellefair" rel="stylesheet" type="text/css"></link>
-                <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed " rel="stylesheet" type="text/css"></link>
+                <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed" rel="stylesheet" type="text/css"></link>
+                <link href="https://fonts.googleapis.com/css?family=Barlow" rel="stylesheet" type="text/css"></link>
             </Head>
             <body>
                 <Main />
