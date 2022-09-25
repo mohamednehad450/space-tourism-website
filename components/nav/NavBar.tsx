27b14c8156cedc29ed2cf0ceeb2e762cff766fc6
Mohamed Nehad <mohammednehad550@gmail.com>
2022-09-25 11:47:24 +0200

    fix: Fix NavBar z index

--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -5,7 +5,7 @@ import { FC, PropsWithChildren } from "react";
 const NavBar: FC<PropsWithChildren> = ({ children }) => {
 
     return (
-        <nav className='absolute lg:top-10 top-0 lg:pl-14 w-full flex flex-row justify-between' >
+        <nav className='absolute z-10 lg:top-10 top-0 lg:pl-14 w-full flex flex-row justify-between' >
             {children}
         </nav>
     )
