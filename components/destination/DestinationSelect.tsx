be3ecc44f504b79b3d26cb235595b35ddbfa4e59
Mohamed Nehad <mohammednehad550@gmail.com>
2022-09-27 07:10:38 +0200

    fix: Fix keys

--- components/destination/DestinationSelect.tsx
+++ components/destination/DestinationSelect.tsx
@@ -10,18 +10,18 @@ const DestinationSelect: FC = () => {
 
 
     const data = useAppData()
+    const [active, setActive] = useState(0)
 
     if (!data || data === "Error") return null
     if (data === "Loading") return (<div>{"Loading"}</div>)
 
     const { destinations } = data
-    const [active, setActive] = useState(0)
 
     return (
         <section className='flex flex-col lg:flex-row px-6 sm:px-24 lg:px-40 gap-9 lg:gap-0'>
             <div className='lg:flex-1 flex flex-col items-center sm:py-5 lg:py-8'>
                 {destinations.map((d, i) => (
-                    <div className={`${i === active ? "" : "hidden"} w-[175px] sm:w-[300px] lg:w-[445px] motion-safe:animate-fade-in`} >
+                    <div key={d.images.webp} className={`${i === active ? "" : "hidden"} w-[175px] sm:w-[300px] lg:w-[445px] motion-safe:animate-fade-in`} >
                         <Image src={d.images.webp} width={445} height={445} />
                     </div>
                 ))}
@@ -30,7 +30,7 @@ const DestinationSelect: FC = () => {
                 <div className='lg:max-w-md'>
                     <div className='flex gap-8 justify-center lg:justify-start'>
                         {destinations.map((d, i) => (
-                            <TabButton active={active === i} onClick={() => setActive(i)}>
+                            <TabButton key={i} active={active === i} onClick={() => setActive(i)}>
                                 {d.name.toLocaleUpperCase()}
                             </TabButton>
                         ))}
@@ -38,6 +38,7 @@ const DestinationSelect: FC = () => {
                     <div className='py-5 sm:py-9 text-center lg:text-start'>
                         {destinations.map((d, i) => (
                             <DestinationDetails
+                                key={i}
                                 name={d.name}
                                 description={d.description}
                                 distance={d.distance}
