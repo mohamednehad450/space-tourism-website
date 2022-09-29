944758c921bf141d1cfdffcfc1e681f8feb62c80
Mohamed Nehad <mohammednehad550@gmail.com>
2022-09-29 19:19:14 +0200

    fix: Fix Header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -7,8 +7,8 @@ const Header: FC<{ title: string, prefix: string }> = ({ prefix, title }) => {
 
 
     return (
-        <span className='sm:text-start font-h5 text-[28px] tracking-[4.7px] leading-8 text-white flex sm:justify-start justify-center gap-7'>
-            <span className='font-bold opacity-25 text-[15px] sm:text-base'>{prefix}</span>
+        <span className='sm:text-start font-h5 text-[28px] tracking-[4.7px] leading-8 text-white flex sm:justify-start justify-center lg:gap-7 gap-4 '>
+            <span className='font-bold opacity-25 text-[15px] sm:text-[20px] lg:text-[28px]'>{prefix}</span>
             <H5>{title}</H5>
         </span>
     )
