71ad1f860c73a829e881bc07f4eca6caad8989fc
Mohamed Nehad <mohammednehad550@gmail.com>
2022-09-29 19:19:43 +0200

    fix: Fix H5

--- components/shared/H5.tsx
+++ components/shared/H5.tsx
@@ -6,7 +6,7 @@ import { FC, HTMLAttributes, PropsWithChildren } from "react"
 const H5: FC<PropsWithChildren<HTMLAttributes<HTMLHeadingElement>>> = (props) => {
 
     return (
-        <h5 {...{ ...props }} className='text-[15px] sm:text-xl lg:text-[28px] font-h5 tracking-[2.7px] sm:tracking-[4.75px]' />
+        <h5 {...{ ...props }} className='text-[15px] sm:text-[20px] lg:text-[28px] font-h5 tracking-[2.7px] sm:tracking-[4.75px]' />
     )
 }
 
