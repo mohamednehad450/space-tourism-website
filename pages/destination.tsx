6d6290ca90568c8433f1e11eb120d80f1d2a3ba2
Mohamed Nehad <mohammednehad550@gmail.com>
2022-09-29 11:02:12 +0200

    fix: Refactor destination page

--- pages/destination.tsx
+++ pages/destination.tsx
@@ -1,24 +1,26 @@
 import type { NextPage } from 'next'
-import { Container, Header } from '../components/shared'
+import { Header } from '../components/shared'
 import { DestinationSelect } from '../components/destination'
+import { useEffect } from 'react'
 
 
 
 
 
 const Destination: NextPage = () => {
+    useEffect(() => {
+        const container = document.getElementById("container")
+        container?.classList.add("bg-destination-m", "sm:bg-destination-t", "lg:bg-destination", "bg-cover")
+        return () => { container?.classList.remove("bg-destination-m", "sm:bg-destination-t", "lg:bg-destination", "bg-cover") }
+    }, [])
     return (
-        <main className={`
-            h-screen w-screen overflow-scroll
-            bg-destination-m sm:bg-destination-t lg:bg-destination bg-cover
-      `}>
-            <Container>
-                <div className='mb-8 sm:m-10 lg:my-16  lg:mx-40 motion-safe:animate-slide-in'>
-                    <Header prefix='01' title={"Pick your destination".toLocaleUpperCase()} />
-                </div>
-                <DestinationSelect />
-            </Container>
-        </main>
+
+        <>
+            <div className='mb-8 sm:m-10 lg:my-16  lg:mx-40 motion-safe:animate-slide-in'>
+                <Header prefix='01' title={"Pick your destination".toLocaleUpperCase()} />
+            </div>
+            <DestinationSelect />
+        </>
     )
 }
 
