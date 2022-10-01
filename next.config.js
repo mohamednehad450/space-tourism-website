247e846cdc6a7b560fb925adbbcdad991d34f76b
Mohamed Nehad <mohammednehad550@gmail.com>
2022-10-01 14:24:47 +0200

    fix: Add prefix to appData

--- next.config.js
+++ next.config.js
@@ -2,12 +2,14 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-  env: { dataPrefix: isProd ? '/space-tourism-website' : '', },
   reactStrictMode: true,
   swcMinify: true,
   assetPrefix: isProd ? '/space-tourism-website/' : '/',
   images: {
     unoptimized: true
+  },
+  publicRuntimeConfig: {
+    assetsPrefix: isProd ? '/space-tourism-website' : '',
   }
 }
 
