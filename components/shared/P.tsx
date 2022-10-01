6c5de7af51b26fcd5da558c959d277caa5627dc7
Mohamed Nehad <mohammednehad550@gmail.com>
2022-10-01 12:29:57 +0200

    fix: Fix P

--- components/shared/P.tsx
+++ components/shared/P.tsx
@@ -8,7 +8,7 @@ import { FC, HTMLAttributes, PropsWithChildren } from "react"
 const P: FC<PropsWithChildren<HTMLAttributes<HTMLParagraphElement>>> = (props) => {
 
     return (
-        <p {...{ ...props }} className='text-[15px] sm:text-[16px] lg:text-lg font-p' />
+        <p {...{ ...props }} className='text-[15px] sm:text-[16px] lg:text-lg lg:leading-8 font-p' />
     )
 }
 
