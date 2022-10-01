35a65f79624fdfcb28fc430b5814736dec84e3df
Mohamed Nehad <mohammednehad550@gmail.com>
2022-10-01 14:04:11 +0200

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+  env: { dataPrefix: isProd ? '/space-tourism-website' : '', },
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? '/space-tourism-website/' : '/',
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
