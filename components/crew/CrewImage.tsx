388a198e50e5e26bac226aa56244b1b549365927
Mohamed Nehad <mohammednehad550@gmail.com>
2022-10-05 13:27:18 +0200

    fix: Fix CrewImage layout

--- components/crew/CrewImage.tsx
+++ components/crew/CrewImage.tsx
@@ -8,6 +8,7 @@ const CrewImage: FC<{ src: string, show?: boolean }> = ({ src, show }) => {
             className={`
                 ${show ? "animate-slide-in" : "hidden"} 
                 lg:absolute
+                min-h-[225px]
             `}
         >
             <img src={src} className={"lg:max-w-2xl lg:max-h-max sm:max-h-[532px] max-h-56"} />
