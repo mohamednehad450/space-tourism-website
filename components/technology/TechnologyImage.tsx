bd94bb3e032330ae3035d58b10241a8017fe2ddd
Mohamed Nehad <mohammednehad550@gmail.com>
2022-10-05 13:31:54 +0200

    fix: Fix TechnologyImage layout

--- components/technology/TechnologyImage.tsx
+++ components/technology/TechnologyImage.tsx
@@ -3,20 +3,36 @@ import Image from 'next/image'
 
 
 const portraitHeight = 800
-const landscapeHeight = 350
+const landscapeHeight = 500
 
 const TechnologyImage: FC<{ show: boolean, src: { portrait: string, landscape: string } }> = ({ show, src }) => {
 
     const [ratioPortrait, setRatioPortrait] = useState(0.75)
-    const [ratioLandscape, setRatioLandscape] = useState(0.75)
+    const [ratioLandscape, setRatioLandscape] = useState(2.47)
 
     return (
-        <div className={`${show ? "animate-fade-in" : "hidden"} lg:max-w-lg lg:max-h-max sm:max-h-80 lg:mx-0 mx-auto`}>
+        <div
+            className={
+                `${show ? "animate-fade-in" : "hidden"}
+                lg:mx-0 lg:w-auto lg:aspect-auto lg:max-w-lg lg:max-h-max
+                mx-auto w-full aspect-[2.47]`
+            }
+        >
             <span className='hidden lg:block'>
-                <Image src={src.portrait} width={portraitHeight * ratioPortrait} height={portraitHeight} onLoadingComplete={r => setRatioPortrait(r.naturalWidth / r.naturalHeight)} />
+                <Image
+                    src={src.portrait}
+                    width={portraitHeight * ratioPortrait}
+                    height={portraitHeight}
+                    onLoadingComplete={r => setRatioPortrait(r.naturalWidth / r.naturalHeight)}
+                />
             </span>
             <span className='lg:hidden'>
-                <Image src={src.landscape} width={landscapeHeight * ratioLandscape} height={landscapeHeight} onLoadingComplete={r => setRatioLandscape(r.naturalWidth / r.naturalHeight)} />
+                <Image
+                    src={src.landscape}
+                    width={landscapeHeight * ratioLandscape}
+                    height={landscapeHeight}
+                    onLoadingComplete={r => setRatioLandscape(r.naturalWidth / r.naturalHeight)}
+                />
             </span>
         </div>
     )
