7e820bcfd17ab54615b10a38592d6c1d7626ce33
Mohamed Nehad <mohammednehad450@gmail.com>
2023-06-16 09:43:45 +0300

    fix: Fix NavLogo alt and aria label

--- components/nav/NavLogo.tsx
+++ components/nav/NavLogo.tsx
@@ -2,17 +2,18 @@ import Image from "next/image";
 import Link from "next/link";
 import { FC } from "react";
 
-
-
-const NavLogo: FC<{ src: string, href: string }> = ({ src, href }) => {
-
+const NavLogo: FC<{ src: string; href: string }> = ({ src, href }) => {
     return (
         <Link href={href}>
-            <a href='#' className='w-10 sm:w-12 h-10 sm:h-12 m-6 sm:ml-10 lg:ml-0'>
-                <Image src={src} width={48} height={48} />
+            <a
+                href="#"
+                className="w-10 sm:w-12 h-10 sm:h-12 m-6 sm:ml-10 lg:ml-0"
+                aria-label="Home page"
+            >
+                <Image alt="Logo" src={src} width={48} height={48} />
             </a>
         </Link>
-    )
-}
+    );
+};
 
-export default NavLogo
+export default NavLogo;
