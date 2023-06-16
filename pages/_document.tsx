5a6c9f4dc38ff11098ac4fcf173b1ca615d84313
Mohamed Nehad <mohammednehad450@gmail.com>
2023-06-16 09:43:17 +0300

    feat: Add lang to html tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
     return (
-        <Html>
+        <Html lang="en">
             <Head>
-                <link href="https://fonts.googleapis.com/css?family=Bellefair" rel="stylesheet" type="text/css"></link>
-                <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed" rel="stylesheet" type="text/css"></link>
-                <link href="https://fonts.googleapis.com/css?family=Barlow" rel="stylesheet" type="text/css"></link>
+                <link
+                    href="https://fonts.googleapis.com/css?family=Bellefair"
+                    rel="stylesheet"
+                    type="text/css"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css?family=Barlow+Condensed"
+                    rel="stylesheet"
+                    type="text/css"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css?family=Barlow"
+                    rel="stylesheet"
+                    type="text/css"
+                />
             </Head>
             <body>
                 <Main />
                 <NextScript />
             </body>
         </Html>
-    )
+    );
 }
